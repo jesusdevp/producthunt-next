42fffd9d78ce6df5cdab4e0379c9c32b6775560f
Jesus Ramirez <jesusdevelp@gmail.com>
2020-07-21 14:05:49 -0500

    Formulario para Crear Cuentas y CSS

--- pages/crear-cuenta.js
+++ pages/crear-cuenta.js
@@ -1,11 +1,52 @@
 import React from "react";
+import { css } from "@emotion/core";
 import Layout from "../components/layout/Layout";
+import { Formulario, Campo, InputSubmit } from "../components/ui/Formulario";
 
 const CrearCuenta = () => {
   return (
     <div>
       <Layout>
-        <h1>Crear Cuenta</h1>
+        <>
+          <h1
+            css={css`
+              text-align: center;
+              margin-top: 5rem;
+            `}
+          >
+            Crear Cuenta
+          </h1>
+          <Formulario>
+            <Campo>
+              <label htmlFor="nombre">Nombre</label>
+              <input
+                type="text"
+                id="nombre"
+                placeholder="Tu Nombre"
+                name="nombre"
+              />
+            </Campo>
+            <Campo>
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                id="email"
+                placeholder="Tu Email"
+                name="email"
+              />
+            </Campo>
+            <Campo>
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                placeholder="Tu Password"
+                name="password"
+              />
+            </Campo>
+            <InputSubmit type="submit" value="Crear Cuenta" />
+          </Formulario>
+        </>
       </Layout>
     </div>
   );
