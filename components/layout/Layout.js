50a1a33c22b8a3eadc4b811f0ac705e1bc282d01
Jesus Ramirez <jesusdevelp@gmail.com>
2020-07-21 01:25:21 -0500

    Primeros ajustes e instalacion de dependencias

---
+++ components/layout/Layout.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Link from "next/link";
+
+const Layout = (props) => {
+  return (
+    <>
+      <h1>Header</h1>
+      <nav>
+        <Link href="/">Inicio</Link>
+      </nav>
+      <main>{props.children}</main>
+    </>
+  );
+};
+
+export default Layout;
