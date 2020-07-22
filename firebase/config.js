fcf1455589a0e83aef0fa595cf42831e65385b13
Jesus Ramirez <jesusdevelp@gmail.com>
2020-07-22 16:02:31 -0500

    Archivos de configuracion de Firebase y Context de Firebase

---
+++ firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCB-UXwuz5k9XyF1EfCF8L8Sy38ihD5gMY",
+  authDomain: "product-hunt-1425e.firebaseapp.com",
+  databaseURL: "https://product-hunt-1425e.firebaseio.com",
+  projectId: "product-hunt-1425e",
+  storageBucket: "product-hunt-1425e.appspot.com",
+  messagingSenderId: "556056031200",
+  appId: "1:556056031200:web:d32d4c613c20b71f780585",
+};
+
+export default firebaseConfig;
