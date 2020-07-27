dea3a0f5d6241ff62dba3959c7f65ae1bbed50e9
Jesus Ramirez <jesusdevelp@gmail.com>
2020-07-26 20:12:49 -0500

    Consultando la base de datos

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { useEffect, useState, useContext } from "react";
 import Layout from "../components/layout/Layout";
-import styled from "@emotion/styled";
+import { FirebaseContext } from "../firebase";
+import firebaseConfig from "../firebase/config";
 
 const Home = () => {
+  const [productos, guardarProductos] = useState([]);
+
+  const { firebase } = useContext(FirebaseContext);
+
+  useEffect(() => {
+    const obtenerProductos = () => {
+      firebase.db
+        .collection("productos")
+        .orderBy("creado", "desc")
+        .onSnapshot(manejarSnapshot);
+    };
+    obtenerProductos();
+  }, []);
+
+  function manejarSnapshot(snapshot) {
+    const productos = snapshot.docs.map((doc) => {
+      return {
+        id: doc.id,
+        ...doc.data(),
+      };
+    });
+    console.log(productos);
+  }
+
   return (
     <div>
       <Layout>
