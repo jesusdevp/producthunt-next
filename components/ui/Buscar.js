fbcc8891e912278282808008832b93088baff6d4
Jesus Ramirez <jesusdevelp@gmail.com>
2020-07-28 16:50:05 -0500

    Pasando la busqueda al componente

--- components/ui/Buscar.js
+++ components/ui/Buscar.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "@emotion/styled";
 import { css } from "@emotion/core";
+import Router from "next/router";
 
 const InputText = styled.input`
   border: 1px solid var(--gris3);
@@ -28,13 +29,32 @@ const InputSubmit = styled.button`
 `;
 
 const Buscar = () => {
+  const [busqueda, guardarBusqueda] = useState("");
+
+  const buscarProducto = (e) => {
+    e.preventDefault();
+
+    if (busqueda.trim() === "") return;
+
+    // redireccionar a /buscar
+    Router.push({
+      pathname: "/buscar",
+      query: { q: busqueda },
+    });
+  };
+
   return (
     <form
       css={css`
         position: relative;
       `}
+      onSubmit={buscarProducto}
     >
-      <InputText type="text" placeholder="Buscar Productos" />
+      <InputText
+        type="text"
+        placeholder="Buscar Productos"
+        onChange={(e) => guardarBusqueda(e.target.value)}
+      />
       <InputSubmit type="submit">Buscar</InputSubmit>
     </form>
   );
--- pages/buscar.js
+++ pages/buscar.js
@@ -1,7 +1,13 @@
 import React from "react";
 import Layout from "../components/layout/Layout";
+import { useRouter } from "next/router";
 
 const Buscar = () => {
+  const router = useRouter();
+  const {
+    query: { q },
+  } = router;
+
   return (
     <div>
       <Layout>
